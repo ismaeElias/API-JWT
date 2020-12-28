a76bacc2fe16b9c0da170359757684b47b7ec58d
Ismael Elias <51543534+ismaeElias@users.noreply.github.com>
2020-12-28 12:25:22 -0300

    implementando auth login

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const {estrategiasAuth} = require('./src/usuarios');
+
+
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+module.exports = app;
---
+++ src/usuarios/usuarios-rotas.js
@@ -0,0 +1,14 @@
+const usuariosControlador = require('./usuarios-controlador');
+const passport = require('passport');
+
+module.exports = app => {
+  app
+     .route('/usuario/Login')
+     .post(passport.authenticate('local', { session : false }), usuariosControlador.login);
+  app
+    .route('/usuario')
+    .post(usuariosControlador.adiciona)
+    .get(usuariosControlador.lista);
+
+  app.route('/usuario/:id').delete(passport.authenticate('bearer', {session : false}),usuariosControlador.deleta);
+};
